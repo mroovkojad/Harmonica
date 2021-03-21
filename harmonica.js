d19b9aaa3a3026081e6585dee9ec41dd431cf4cd
Michał Wojewoda <michal.wojewoa@hotmail.com>
2021-03-21 12:58:55 +0100

    finished

--- harmonica.js
+++ harmonica.js
@@ -1,45 +1,34 @@
-var data;
+$.getJSON("richter.json", function (json) {
+  var body = document.body,
+    tbl = document.createElement("table"),
+    harmonica = json;
 
-try {
-   articulaions = JSON.parse(fs.readFileSync('richter.json'));
-} catch ( err ) {
-   // handle your file not found (or other error) here
-}
-
-function tableCreate() {
-  var height = 8,
-    length = 10,
-    body = document.body,
-    tbl = document.createElement("table");
-  tbl.style.border = 'hidden'
-
-  var k = 0
-  for (var i = 0; i < height; i++) {
+  var k = 0;
+  var row = harmonica.articulations[k].row;
+  var hole = harmonica.articulations[k].hole - 1;
+  var value = harmonica.articulations[k].value;
+  var total_articulations = harmonica.articulations.length;
+  var technique = harmonica.articulations[k].technique;
+  for (var i = 0; i < harmonica.rows; i++) {
     var tr = tbl.insertRow();
-    for (var j = 0; j < length; j++) {
+    tr.style.height  = "40px";
+    for (var j = 0; j < harmonica.holes; j++) {
       var td = tr.insertCell();
-      td.style.width = '30px'
-      td.style.height = '30px'
-      td.style.border = '1px solid black';
-
-      if (i==3 ){
-        td.appendChild(document.createTextNode(articulaions[k].value));
-        k++
-      } else{
-        td.style.visibility =  'hidden';
+      td.style.width = "30px";
+      td.style.textAlign = "center"
+      if (i == row && j == hole) {
+        td.appendChild(document.createTextNode(value));
+        td.style.border = "1px solid black";
+        td.className = technique;
+        k++;
+        if (k < total_articulations) {
+          row = harmonica.articulations[k].row;
+          hole = harmonica.articulations[k].hole - 1;
+          technique = harmonica.articulations[k].technique;
+          var value = harmonica.articulations[k].value;
+        }
       }
-
-      //    break;
-      //} else {
-      //    var td = tr.insertCell();
-      //    td.appendChild(document.createTextNode('Cell'));
-      //    td.style.border = '1px solid black';
-      //    if(i == 1 && j == 1){
-      //        td.setAttribute('rowSpan', '2');
-      //    }
-      //}
+      body.appendChild(tbl);
     }
   }
-  body.appendChild(tbl);
-}
-tableCreate();
+});
